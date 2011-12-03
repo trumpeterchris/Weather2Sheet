66d70d7ce7827958b28cf98ef2cf01eb8586bd1a
Stephen Ramsay <sramsay.unl@gmail.com>
2011-12-03 10:43:31 -0600

    Bumped version to 3.0.0.

--- wu/wu.go
+++ wu/wu.go
@@ -30,7 +30,7 @@ const defaultStation = "KLNK"
 
 // GetVersion returns the version of the package
 func GetVersion() string {
-	return "2.0.4"
+	return "3.0.0"
 }
 
 // GetConf returns the API key and weather station from
