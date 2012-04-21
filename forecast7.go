e9840ced9cf41a1faeae5c26197a80544646799f
Stephen Ramsay <sramsay.unl@gmail.com>
2012-04-21 14:40:55 -0500

    Removed reundant data structure (now singly represented in forecast.go).

--- forecast7.go
+++ forecast7.go
@@ -32,25 +32,9 @@ import (
 	"fmt"
 )
 
-//type ForecastConditions struct {
-//	Forecast Forecast
-//}
-//
-//type Forecast struct {
-//	Txt_forecast Txt_forecast
-//}
-//
-//type Txt_forecast struct {
-//	Date        string
-//	Forecastday []Forecastday
-//}
-//
-//type Forecastday struct {
-//	Title   string
-//	Fcttext string
-//}
 
 // printForecast prints the forecast for a given station to standard out
+// The dat structure on which it depends is in forecast.go.
 func PrintForecast7(obs *ForecastConditions, stationId string) {
 	t := obs.Forecast.Txt_forecast
 	fmt.Printf("Forecast for %s\n", stationId)
